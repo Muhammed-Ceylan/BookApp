62a726bf25efa6e094dca77b4419ea9f93f8b807
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2023-11-26 17:04:34 +0300

    Added permission parameter in user_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -16,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('permission')->default(0); //admin mi kullanıcı mı
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
