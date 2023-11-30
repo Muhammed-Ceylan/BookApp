cef1ab20a83e654eedee6cc19c736ef5c7ff3b6e
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2023-12-01 00:50:20 +0300

    Created author's index file.

---
+++ resources/views/admin/author/index.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-12">
+                <div class="card my-4">
+                    <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                        <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                            <h6 class="text-white text-capitalize ps-3">Yazar Listesi</h6>
+                        </div>
+                    </div>
+                    <div class="card-body px-0 pb-2">
+                        <div class="table-responsive p-0">
+                            <table class="table align-items-center mb-0">
+                                <tbody>
+                                @foreach($data as $key => $value)
+                                    <tr>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <img class="mb-0 text-sm" alt="">{{$value->image}}</img>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <h6 class="mb-0 text-sm">{{$value->name}}</h6>
+                                                    <p class="mb-0 text-sm">{{$value->bio}}</p>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <a href="{{route('admin.author.edit',['id'=>$value->id])}}" class="mb-0 text-sm">
+                                                        Düzenle
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="d-flex px-2 py-1">
+                                                <div class="d-flex flex-column justify-content-center">
+                                                    <a href="{{route('admin.author.delete',['id'=>$value->id])}}" class="mb-0 text-sm">
+                                                        Sil
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            {{$data->links()}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
