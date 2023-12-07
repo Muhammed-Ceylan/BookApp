efa3a8f075092954880349403ebe32d73f617643
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2023-12-07 23:05:09 +0300

    Added front blade file for index page and route's for front page.

--- app/Http/Controllers/front/indexController.php
+++ app/Http/Controllers/front/indexController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class indexController extends Controller
 {
-    //
+    public function index(){
+        return view('front.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\front\indexController::class, 'index'])->name('index');
 Route::group(['namespace' => 'admin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('/', [indexController::class, 'index'])->name('index');
     Route::group(['namespace' => 'publisher', 'prefix' => 'publisher', 'as' => 'publisher.'], function () {
