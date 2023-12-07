638d99495259ab0bb2c83166904686ba83e64b91
@Muhammed-Ceylann <muhammed.ceylan@robosay.com.tr>
2023-12-08 00:49:27 +0300

    Updated sidebar and front controller and blade file.

--- app/Http/Controllers/front/indexController.php
+++ app/Http/Controllers/front/indexController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 
 class indexController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('front.index');
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -56,6 +56,14 @@
                     <span class="nav-link-text ms-1">Kategoriler</span>
                 </a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="{{route('admin.slider.index')}}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">person</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Slider</span>
+                </a>
+            </li>
         </ul>
     </div>
 </aside>
